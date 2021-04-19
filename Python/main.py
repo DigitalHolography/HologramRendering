b1137639bc61e79b8e4d393b8adfed381d06e0f6
Julia Sverdlin <sverdlj@essilor.fr>
2021-04-19 18:22:08 +0200

    Updated main.py

--- Python/main.py
+++ Python/main.py
@@ -1,20 +1,32 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-
-# image = np.array
-# f = open("demofile.txt", "r")
-
+from matplotlib import pyplot as plt
 import numpy as np
-from PIL import Image
+from io import StringIO
+from PIL import *
+import Hologram
 
-array = np.zeros([100, 200, 3], dtype=np.uint8)
-array[:,:100] = [255, 128, 0] #Orange left side
-array[:,100:] = [0, 0, 255]   #Blue right side
 
-img = Image.fromarray(array)
-img.save('testrgb.png')
+z = 0.006
+ImageArraySize = np.array([2048, 2048])
+nx = ImageArraySize[0]
+ny = ImageArraySize[1]
+
+
+img_infile = open('GaborTarget_SingleImage_XY_D_2048_2048_8bit_e.raw','rb')
+img_array = np.fromfile(img_infile, dtype=np.uint8, count=nx*ny)
+img = Image.frombuffer("I", [nx, ny],
+                            img_array.astype('I'),
+                            'raw','I', 0, 1)
+plt.imshow(img)
+plt.show()
+
+tab_z = np.linspace(0.01, 0.3, 10)
+pp = np.zeros()
+pp = np.arange(tab_z.size - 1)
+z = tab_z[pp]
+[OutputField] = Hologram.hologram2FFT(img, nx, ny, 5.5e-6, 5.5e-6, 658e-9, -z)
+DisplayedImage = np.abs(OutputField)
+
+plt.imshow(DisplayedImage)
+
+plt.imshow(np.log(np.abs(np.fft.fftshift(np.fft.fft2(img)))))
+
