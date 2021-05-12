41863b4d361b5c2824327b854c15c6bfec474e54
Julia Sverdlin <sverdlj@essilor.fr>
2021-05-12 13:49:33 +0200

    demodulation temporelle

--- Python/main.py
+++ Python/main.py
@@ -5,8 +5,7 @@ from PIL import Image
 import Hologram
 import holo
 import struct
-
-
+import math
 
 # z = 0.06
 
@@ -20,23 +19,34 @@ nx, ny = ImageArraySize
 # img = Image.frombuffer("I", [nx, ny],
 #                             img_array.astype('I'),
 #                             'raw','I', 0, 1)
+#
+# img = holo.FileReader('Mire_negative_position_73.holo').get_all_frames()
 
-#img = holo.FileReader('Mire_negative_position_73.holo').get_all_frames()
-
-data = holo.FileReader('Mire_negative_position_160.holo').get_all_frames() #ici tu mets le nom du fichier que tu veux ouvrir
-data_reshape = np.reshape(data[:,0], (1024, 1024), order = 'C') #dans data[:, 0] ça veut dire que tu prends la première image du film (correspond à l'indice 0)
+data = holo.FileReader('Mire_negative_position_73.holo').get_all_frames() #ici tu mets le nom du fichier que tu veux ouvrir
+data_reshape_1 = np.reshape(data[:, 0], (1024, 1024), order='C') #dans data[:, 0] ça veut dire que tu prends la première image du film (correspond à l'indice 0)
                                                               #(1024, 1024) c'est les dimensions des images, attention il faut les choisir intelligement les dimensions sont écrits dans le header normalement
 # plt.imshow(np.rot90(np.flipud(data_reshape), 4), aspect='auto', interpolation='none', origin='lower') #ça c'est juste pour afficher
 # plt.show()
-
-
+data_reshape_2 = np.reshape(data[:, 1], (1024, 1024), order='C')
+data_reshape_3 = np.reshape(data[:, 2], (1024, 1024), order='C')
+data_reshape_4 = np.reshape(data[:, 3], (1024, 1024), order='C')
+data_reshape_1 = data_reshape_1.astype("complex128")
+data_reshape_2 = data_reshape_2.astype("complex128")
+data_reshape_3 = data_reshape_3.astype("complex128")
+data_reshape_4 = data_reshape_4.astype("complex128")
 # plt.imshow(data_reshape)
 # plt.title('interferogram')
 # plt.show()
 
+# Démodulation temporelle
+I0 = 1/4 * (data_reshape_1 + data_reshape_2 + data_reshape_3 + data_reshape_4)
+I1 = 1/4 * 1j * (data_reshape_3 - data_reshape_1) + 1/4 * (data_reshape_4 - data_reshape_2)
+I2 = 1/4 * (-data_reshape_1 + data_reshape_2 - data_reshape_3 + data_reshape_4)
+I3 = 1/4 * 1j * (data_reshape_1 - data_reshape_3) + 1/4 * (data_reshape_4 - data_reshape_2)
+
 
 for z in np.linspace(0.01, 0.3, 10):
-    OutputField = Hologram.hologram1FFT(data_reshape, nx, ny, 5.5e-6, 5e-6, 658e-9, -z)
+    OutputField = Hologram.hologram1FFT(I2, nx, ny, 5.5e-6, 5e-6, 658e-9, -z)
     # OutputField = Hologram.hologram1FFT(img, nx, ny, 5.5e-6, 5.5e-6, 658e-9, -z)
     DisplayedImage = np.absolute(OutputField)
     plt.imshow(DisplayedImage)
