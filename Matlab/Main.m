0740f4dddb44df43fdf6bf3817e7701df740db46
emmanuelmartser <81562609+emmanuelmartser@users.noreply.github.com>
2021-05-17 18:33:35 +0200

    Main.m

---
+++ Matlab/Main.m
@@ -0,0 +1,45 @@
+%dummy text
+z = 0.06;
+ImageArraySize = [2048,2048];
+Nx = ImageArraySize(1);
+Ny = ImageArraySize(2);
+OriginDirectory = './';
+CurrentImageName = 'GaborTarget_SingleImage_XY_D_2048_2048_8bit_e.raw';
+%CurrentImageName = 'worm2_z0-05_XY_D_2048_2048_8bit_e.raw';
+
+fid = fopen ([OriginDirectory,CurrentImageName], 'r');
+%fseek(fid,Nshift*Nx*Ny,'bof'); %decalage de Nshift images
+CurrentImage = fread(fid,Nx*Ny,'uint8=>uint8'); 
+fclose(fid);
+
+CurrentImage = double(CurrentImage);
+CurrentImage = reshape(CurrentImage,Nx,Ny);
+
+figure(1)
+imagesc(abs(CurrentImage));
+axis image;
+axis off;
+colormap(gray.^0.5);
+title('interferogram');
+
+tab_z = linspace(0.01,0.3,10);
+% tab_z = 0.005
+for pp = 1:length(tab_z)
+z = tab_z(pp);
+[OutputField] = HologramRendering(CurrentImage,'2FFT',Nx,Ny,5.5e-6,5.5e-6,658e-9,-z);
+DisplayedImage = abs(OutputField);
+figure(2)
+imagesc(DisplayedImage);
+axis image;
+axis off;
+colormap(gray.^0.5);
+title('reconstructed hologram');
+pause(0.01);
+end%pp
+
+figure(3)
+imagesc(log(abs(fftshift(fft2(CurrentImage)))));
+axis image;
+axis off;
+colormap(gray.^0.5);
+title('Fourier plane');
