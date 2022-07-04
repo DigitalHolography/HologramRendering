c523900aa07d44309917da0479e2917da61d5221
Benjamin Besnainou & Aurelien Duval <benjamin.besnainou@epitech.eu>
2022-07-04 16:59:10 +0200

    [FIX] running-directory

--- Matlab/Main.m
+++ Matlab/Main.m
@@ -3,13 +3,27 @@ z = 0.06;
 ImageArraySize = [2048,2048];
 Nx = ImageArraySize(1);
 Ny = ImageArraySize(2);
-OriginDirectory = './';
+
+pathparts = strsplit(pwd,filesep);
+
+if ~(strcmp(pathparts(end-1), "HologramRendering"))
+    if ~(strcmp(pathparts(end), "Matlab"))
+     %   disp("Please run this script from HologramRendering/Matlab/")
+     f = msgbox("Please run this script from HologramRendering/Matlab/", "Invalid Folder", "error");
+        return
+    end
+end
+
+OriginDirectory = '../Images/';
 CurrentImageName = 'GaborTarget_SingleImage_XY_D_2048_2048_8bit_e.raw';
+
 %CurrentImageName = 'worm2_z0-05_XY_D_2048_2048_8bit_e.raw';
 
-fid = fopen ([OriginDirectory,CurrentImageName], 'r');
+
+
+fid = fopen([OriginDirectory,CurrentImageName], 'r');
 %fseek(fid,Nshift*Nx*Ny,'bof'); %decalage de Nshift images
-CurrentImage = fread(fid,Nx*Ny,'uint8=>uint8'); 
+CurrentImage = fread(fid,Nx*Ny,'uint8=>uint8');
 fclose(fid);
 
 CurrentImage = double(CurrentImage);
@@ -21,7 +35,7 @@ axis image;
 axis off;
 colormap(gray.^0.5);
 title('interferogram');
-
+ 
 tab_z = linspace(0.01,0.3,10);
 % tab_z = 0.005
 for pp = 1:length(tab_z)
